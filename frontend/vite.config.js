cc2a01aecd92dc0538d896cdd8ec4985b0e217f8
Abhinav Thakur <thakurabhinav16160@gmail.com>
2025-04-02 16:53:00 +0530

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173, // Render will override this with $PORT
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173, // Render will override this with $PORT
+    strictPort: true,
+    allowedHosts: ["chatapp-0.onrender.com"], // Add your Render domain here
+  },
+});
